f2672533985d4216c699601db2733416400528db
Brian Duemmer <bduemmer1@gmail.com>
2018-03-15 16:06:49 -0400

    Added CAN communications programs to repo
    Added code for the arduino to print data to the CAN bus from the IMU hat
    / sensors, and java code to read the bus and extract al the data

---
+++ CAN_com/CanParser/src/edu/njit/solarcar/telemetry/CanTest/CanReader.java
@@ -0,0 +1,273 @@
+package edu.njit.solarcar.telemetry.CanTest;
+
+import java.io.IOException;
+
+import javax.xml.bind.DatatypeConverter;
+
+import de.entropia.can.CanSocket;
+import de.entropia.can.CanSocket.CanFrame;
+import de.entropia.can.CanSocket.CanInterface;
+import de.entropia.can.CanSocket.Mode;
+
+public class CanReader 
+{
+	private CanSocket sock;
+	
+	private boolean shutdown;
+	
+	// gravity acceleration
+	private double gaccX;
+	private double gaccY;
+	private double gaccZ;
+	
+	// linear acceleration
+	private double laccX;
+	private double laccY;
+	private double laccZ;
+	
+	// gyroscope
+	private double gyroX;
+	private double gyroY;
+	private double gyroZ;
+	
+	//Misc. Arduino
+	private double potVal;
+	
+	// BMS data
+	private double packCurrent;
+	private double packInstVolts;
+	private double packSoc;
+	private double relayState;
+	private double packDCL;
+	private double packTemp;
+	
+	
+
+	public CanReader(boolean startShellModule) throws IOException {
+		if(startShellModule) {
+			Runtime.getRuntime().exec("sudo sh ~/initCan.sh");
+		}
+		sock = new CanSocket(Mode.RAW);
+		sock.bind(new CanInterface(sock, "can0"));
+	}
+	
+	
+	/**
+	 * Starts a daemon to continuously grab any buffered CAN data
+	 * @param pollRate
+	 */
+	public void startPollingLoop(long pollRate) {
+		shutdown = false;
+		Thread t = new Thread(() -> {
+			while(!shutdown) {
+				try {
+					CanFrame frame = sock.recv();
+					if(frame != null)
+						parseFrame(frame);
+					
+					if(pollRate > 0) // only delay if needed
+						Thread.sleep(pollRate);
+				} catch (Exception e) {
+//					qshutdown = true;
+					e.printStackTrace();
+				}
+			}
+		});
+		t.setDaemon(true);
+		t.setName("CAN Polling");
+		t.start();
+	}
+	
+	
+	/**
+	 * Prints a frame to stout
+	 * @param frame
+	 */
+	public void dumpFrame(CanFrame frame) {
+		int id = frame.getCanId().getCanId_SFF();
+		byte[] data = frame.getData();
+		String hexDat = DatatypeConverter.printHexBinary(data);
+		System.out.printf("Frame => id:%X data: %s\n", id, hexDat);
+	}
+	
+	
+	public void parseFrame(CanFrame frame) {
+		int id = frame.getCanId().getCanId_SFF();
+		byte[] data = frame.getData();
+		
+		switch(id) { // extract the proper frame info, update the respective variables
+			case 0x6B0: { // BMS 1
+				packCurrent = (double)data[0];
+				packInstVolts = (double)data[2];
+				packSoc = (double)data[4];
+				relayState = (double)data[7];
+				break;
+			}	
+			case 0x6B1: { // BMS 2
+				packDCL = (double)data[0];
+				packTemp = (double)(((int)data[4] << 8) + data[5]);
+				break;
+			}
+			case 0x100: { // Arduino Gravity Acceleration
+				gaccX = ((double)(((int)data[0] << 8) + data[1])) / 256;
+				gaccY = ((double)(((int)data[2] << 8) + data[3])) / 256;
+				gaccZ = ((double)(((int)data[4] << 8) + data[5])) / 256;
+				break;
+			}
+			case 0x101: { // Arduino Linear Acceleration
+				laccX = ((double)(((int)data[0] << 8) + data[1])) / 256;
+				laccY = ((double)(((int)data[2] << 8) + data[3])) / 256;
+				laccZ = ((double)(((int)data[4] << 8) + data[5])) / 256;
+				break;
+			}
+			case 0x102: { // Arduino Gyro
+				gyroX = ((double)(((int)data[0] << 8) + data[1])) / 256;
+				gyroY = ((double)(((int)data[2] << 8) + data[3])) / 256;
+				gyroZ = ((double)(((int)data[4] << 8) + data[5])) / 256;
+				break;
+			}
+			case 0x103: { // Arduino Sensors
+				potVal = ((double)(((int)data[0] << 8) + data[1])) / 32; // it's premultiplied before sending, so don't need to divide by full 256
+				break;
+			}
+			default: {
+				System.err.println("Unknown Frame ID: " +id);
+				break;
+			}
+		}
+	}
+
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("CanReader [gaccX=");
+		builder.append(gaccX);
+		builder.append(", gaccY=");
+		builder.append(gaccY);
+		builder.append(", gaccZ=");
+		builder.append(gaccZ);
+		builder.append(", laccX=");
+		builder.append(laccX);
+		builder.append(", laccY=");
+		builder.append(laccY);
+		builder.append(", laccZ=");
+		builder.append(laccZ);
+		builder.append(", gyroX=");
+		builder.append(gyroX);
+		builder.append(", gyroY=");
+		builder.append(gyroY);
+		builder.append(", gyroZ=");
+		builder.append(gyroZ);
+		builder.append(", packCurrent=");
+		builder.append(packCurrent);
+		builder.append(", packInstVolts=");
+		builder.append(packInstVolts);
+		builder.append(", packSoc=");
+		builder.append(packSoc);
+		builder.append(", relayState=");
+		builder.append(relayState);
+		builder.append(", packDCL=");
+		builder.append(packDCL);
+		builder.append(", packTemp=");
+		builder.append(packTemp);
+		builder.append(", potVal=");
+		builder.append(potVal);
+		builder.append("]");
+		return builder.toString();
+	}
+
+
+	public double getGaccX() {
+		return gaccX;
+	}
+
+
+	public double getGaccY() {
+		return gaccY;
+	}
+
+
+	public double getGaccZ() {
+		return gaccZ;
+	}
+
+
+	public double getLaccX() {
+		return laccX;
+	}
+
+
+	public double getLaccY() {
+		return laccY;
+	}
+
+
+	public double getLaccZ() {
+		return laccZ;
+	}
+
+
+	public double getGyroX() {
+		return gyroX;
+	}
+
+
+	public double getGyroY() {
+		return gyroY;
+	}
+
+
+	public double getGyroZ() {
+		return gyroZ;
+	}
+
+
+	public double getPotVal() {
+		return potVal;
+	}
+
+
+	public double getPackCurrent() {
+		return packCurrent;
+	}
+
+
+	public double getPackInstVolts() {
+		return packInstVolts;
+	}
+
+
+	public double getPackSoc() {
+		return packSoc;
+	}
+
+
+	public double getRelayState() {
+		return relayState;
+	}
+
+
+	public double getPackDCL() {
+		return packDCL;
+	}
+
+
+	public double getPackTemp() {
+		return packTemp;
+	}
+	
+	
+	
+
+}
+
+
+
+
+
+
+
+
+
+
---
+++ CAN_com/arduino/imuCanWrite/imuCanWrite.ino
@@ -0,0 +1,118 @@
+// demo: CAN-BUS Shield, send data
+// loovee@seeed.cc
+
+#include <mcp_can.h>
+#include <SPI.h>
+
+#include "NineAxesMotion.h"
+#include <Wire.h>
+
+#define CAN_BUF_SIZE 8
+
+NineAxesMotion imu;
+const int SPI_CS_PIN = 9;
+MCP_CAN CAN(SPI_CS_PIN);                                    // Set CS pin
+
+unsigned char canBuf[CAN_BUF_SIZE];
+
+void fmtSendCanFrame(float x, float y, float z, unsigned char* buf, unsigned int id);
+
+float laccX;
+float laccY;
+float laccZ;
+
+float gaccX;
+float gaccY;
+float gaccZ;
+
+float gyroX;
+float gyroY;
+float gyroZ;
+
+float potVal;
+
+
+void setup()
+{
+    Serial.begin(115200);
+
+    while (CAN_OK != CAN.begin(CAN_500KBPS))              // init can bus : baudrate = 500k
+    {
+        Serial.println("CAN BUS Shield init fail");
+        Serial.println(" Init CAN BUS Shield again");
+        delay(100);
+    }
+    Serial.println("CAN BUS Shield init ok!");
+
+    
+    I2C.begin();
+    imu.initSensor();
+    imu.setOperationMode(OPERATION_MODE_NDOF);
+    imu.setUpdateMode(AUTO);
+}
+
+
+void loop()
+{
+  // Read the 9 axis data in
+  imu.readGravAccel(gaccX, gaccY, gaccZ);
+  imu.readLinearAccel(laccX, laccY, laccZ);
+  imu.readGyro(gyroX, gyroY, gyroZ);
+
+  potVal = (float)analogRead(A0)/8;
+  Serial.println(potVal);
+  
+  fmtSendCanFrame(gaccX, gaccY, gaccZ, canBuf, 0x100);
+  fmtSendCanFrame(laccX, laccY, laccZ, canBuf, 0x101);
+  fmtSendCanFrame(gyroX, gyroY, gyroZ, canBuf, 0x102);
+  fmtSendCanFrame(potVal, 0, 0, canBuf, 0x103);
+
+  delay(250);
+}
+
+
+
+
+void fmtSendCanFrame(float x, float y, float z, unsigned char* buf, unsigned int id) {
+  for(int i=0; i<CAN_BUF_SIZE; i++) {
+    canBuf[i] = 0;
+  }
+  
+  int ix = (int)(x*256.0);
+  int iy = (int)(y*256.0);
+  int iz = (int)(z*256.0);
+
+  buf[0] = ix >> 8;
+  buf[1] = ix & 0xFF;
+  buf[2] = iy >> 8;
+  buf[3] = iy & 0xFF;
+  buf[4] = iz >> 8;
+  buf[5] = iz & 0xFF;
+  
+  Serial.print("Sent Frame: ");
+  Serial.print(id);
+  Serial.print(", ");
+  for(int i=0; i<CAN_BUF_SIZE; i++) {
+    Serial.print(buf[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.println();
+  
+  CAN.sendMsgBuf(id, 0, 8, buf);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// END FILE
