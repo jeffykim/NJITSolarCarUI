b67528edf337d82b70f6720b516e01ab94c42342
nayeemkamal <33039967+nayeemkamal@users.noreply.github.com>
2018-04-07 18:32:54 +0000

    udp

--- UDPReciever/src/UDPReciever.java
+++ UDPReciever/src/UDPReciever.java
@@ -12,28 +12,28 @@ public class UDPReciever extends Thread
 {
 	private static final int BUF_SIZE = 1024;
 	private static byte[] recvBuf = new byte[BUF_SIZE];
-	
-	private static final String addr = "localhost";
+
+	private static final String addr = "solarcar-telemetry-raspi.local";
 	private static final int port = 7888;
 	private static InetAddress serverIp;
 	private static DatagramSocket sock;
-	
+
 	public static void main(String[] args) {
 		UDPReciever r = new UDPReciever();
 		r.run();
 	}
 
 	public UDPReciever()  {
-		
-		
+
+
 		try {
-			
+
 			BufferedReader userIn = new BufferedReader(new InputStreamReader(System.in));
 			sock = new DatagramSocket(port);
 			serverIp = InetAddress.getByName(addr);
-			
+
 		} catch (Exception e) {
-	        e.printStackTrace();
+			e.printStackTrace();
 		}
 	}
 	/**
@@ -49,23 +49,27 @@ public class UDPReciever extends Thread
 
 
 	public void run(){
+
+
+		try{
+			sendData("fuck you"); // init the remote IP
+			byte[] receiveData = new byte[256];
+			byte[] sendData = new byte[256];
+
+			while(true)
+			{
+				DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+				sock.receive(receivePacket);
+				String sentence = new String( receivePacket.getData());
+				System.out.println("RECEIVED: " + sentence);
+				InetAddress IPAddress = receivePacket.getAddress();
+
+			}
+		}catch (Exception e){
+		}
 		
-
-	    try{
-	        
-	        byte[] receiveData = new byte[8];
-	        byte[] sendData = new byte[8];
-
-	        while(true)
-	           {
-	              DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-	              sock.receive(receivePacket);
-	              String sentence = new String( receivePacket.getData());
-	              System.out.println("RECEIVED: " + sentence);
-	              InetAddress IPAddress = receivePacket.getAddress();
-	             
-	           }
-	    }catch (Exception e){
-	    }
+		
+		
+		
 	}
 }
--- UDPSender/src/UDPSender.java
+++ UDPSender/src/UDPSender.java
@@ -9,35 +9,37 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @SuppressWarnings("unused")
 public class UDPSender extends Thread
 {
 	private static final int BUF_SIZE = 1024;
 	private static byte[] recvBuf = new byte[BUF_SIZE];
-	
-	private static final String addr = "localhost";
+
+	private static final String addr = "solarcar-telemetry-raspi.local";
 	private static final int port = 7888;
 	private static InetAddress serverIp;
 	private static DatagramSocket sock;
 
-	
+
 	public static void main(String[] args) {
 		UDPSender s = new UDPSender();
 		s.run();
 	}
-	
-	
+
+
 	public UDPSender()  {
-		
+
 		try {
-		
+
 			BufferedReader userIn = new BufferedReader(new InputStreamReader(System.in));
 			sock = new DatagramSocket();
 			serverIp = InetAddress.getByName(addr);
-			
+
 		} catch (Exception e) {
-	      e.printStackTrace();
+			e.printStackTrace();
 		}
 	}
 	/**
@@ -54,12 +56,15 @@ public class UDPSender extends Thread
 
 	@Override
 	public void run() {
-		try {
-		UDPSender.sendData("Hello");
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
+		while(true) {
+			try {
+				UDPSender.sendData(String.format("Hello! Time is:%s\n", new SimpleDateFormat("yyyy.MM.dd - HH.mm.ss").format(new Date())));
+				Thread.sleep(1000);
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+			}
+		}	
 	}
 
 }
